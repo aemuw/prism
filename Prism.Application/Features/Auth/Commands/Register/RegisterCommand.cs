b25ebc78712114014f20262ae2c8c61f17e18282
Artem <artembohun@gmail.com>
2026-03-22 21:06:51 +0200

    feat: add Auth DTOs and RegisterCommand

---
+++ Prism.Application/Features/Auth/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Prism.Application.DTOs.Auth;
+
+namespace Prism.Application.Features.Auth.Commands.Register
+{
+    public record RegisterCommand(
+        string Username,
+        string Email,
+        string Password
+    ) : IRequest<AuthResponse>;
+}
