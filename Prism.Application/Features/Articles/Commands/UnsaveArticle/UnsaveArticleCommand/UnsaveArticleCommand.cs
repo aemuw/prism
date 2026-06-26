d8a27bc06692e25784187f502913ed7ea8675d7f
Artem <artembohun@gmail.com>
2026-06-27 00:08:23 +0300

    feat: add SaveArticle and UnsaveArticle commands

---
+++ Prism.Application/Features/Articles/Commands/UnsaveArticle/UnsaveArticleCommand/UnsaveArticleCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism.Application.Features.Articles.Commands.UnsaveArticle.UnsaveArticleCommand
+{
+    internal class _
+    {
+    }
+}
---
+++ Prism.Application/Features/Articles/Commands/UnsaveArticle/UnsaveArticleCommand/UnsaveArticleHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Prism.Application.Exceptions;
+using Prism.Domain.Interfaces;
+
+namespace Prism.Application.Features.Articles.Commands.UnsaveArticle.UnsaveArticleCommand
+{
+    public class UnsaveArticleHandler
+    {
+        private readonly ISavedArticleRepository _savedArticleRepository;
+
+        public UnsaveArticleHandler(ISavedArticleRepository savedArticleRepository)
+        {
+            _savedArticleRepository = savedArticleRepository;
+        }
+
+        public async Task<bool> Handle(
+            UnsaveArticleCommand command, CancellationToken cancellationToken)
+        {
+            var exists = await _savedArticleRepository
+                .ExistsAsync(command.UserId, command.ArticleId);
+
+            if (!exists)
+                throw new NotFoundException("Збереженої статті не знайдено");
+
+            await _savedArticleRepository
+                .DeleteAsync(command.UserId, command.ArticleId);
+
+            return true;
+        }
+    }
+}
