a3c3d50eb5fa18278cca6639851b4bd4b2102463
Artem <artembohun@gmail.com>
2026-09-01 22:45:52 +0300

    chore: add Prism.Infrastructure with EF Core and DbContext skeleton

---
+++ Prism.Infrastructure/Persistence/PrismDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prism.Domain.Entities;
+
+namespace Prism.Infrastructure.Persistence
+{
+    public class PrismDbContext : DbContext
+    {
+        public PrismDbContext(DbContextOptions<PrismDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Source> Sources => Set<Source>();
+        public DbSet<UserSource> UserSources => Set<UserSource>();
+        public DbSet<Article> Articles => Set<Article>();
+        public DbSet<Tag> Tags => Set<Tag>();
+        public DbSet<SavedArticle> SavedArticles => Set<SavedArticle>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PrismDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
