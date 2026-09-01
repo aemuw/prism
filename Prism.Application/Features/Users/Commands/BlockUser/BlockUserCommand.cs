552a2363de688620835d31713133d8a296bbdb12
Artem <artembohun@gmail.com>
2026-09-01 21:22:45 +0300

    feat: add BlockUser command

---
+++ Prism.Application/Features/Users/Commands/BlockUser/BlockUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Prism.Application.Features.Users.Commands.BlockUser
+{
+    public record BlockUserCommand(Guid UserId) : IRequest<bool>;
+}
---
+++ Prism.Application/Features/Users/Commands/BlockUser/BlockUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Prism.Application.Exceptions;
+using Prism.Domain.Interfaces;
+
+namespace Prism.Application.Features.Users.Commands.BlockUser
+{
+    public class BlockUserHandler : IRequestHandler<BlockUserCommand, bool>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public BlockUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> Handle(BlockUserCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(command.UserId);
+
+            if (user is null)
+                throw new NotFoundException("Користувача не знайдено");
+
+            user.Block();
+            await _userRepository.UpdateAsync(user);
+
+            return true;
+        }
+    }
+}
